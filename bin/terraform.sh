e178a04662392c30d21ce7dd5a2c932f06c3a065
Henry Lawson <henry.lawson@foinq.com>
2016-10-27 22:01:29 +0800

    getting started with git crypt

---
+++ bin/terraform.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xeuo pipefail
+
+DIR=$( cd $(dirname $0) ; pwd -P )
+TERRAFORM_COMMAND=$1
+TERRAFORM_COMMANDS=$@
+ROOT_DIR=$DIR/..
+TERRAFORM_CREDS=$ROOT_DIR/keys/creds.tfvars.crypt
+
+cd $ROOT_DIR/infra
+
+terraform remote config \
+    -backend=s3 \
+    -backend-config="bucket=henrylawson.net-terraform-state-production" \
+    -backend-config="key=henrylawson.net/terraform.tfstate" \
+    -backend-config="region=us-east-1"
+
+terraform $TERRAFORM_COMMANDS \
+  -var-file="$TERRAFORM_CREDS"
